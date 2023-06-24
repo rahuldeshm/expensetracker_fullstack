429a9b709a8849b3b07ac90083300bb16e5ed9b3
rahul deshmukh <rahuldeshmukh4545@gmail.com>
2023-06-24 20:48:02 +0530

    added bcrypt encription to save the password when login and also to compare the password with the password saved to login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 function isStringInvalid(string) {
   if (string == undefined || string.length == 0) {
     return true;
@@ -18,30 +19,45 @@ exports.signup = async (req, res, next) => {
       isStringInvalid(password)
     ) {
       return res.status(400).json({ err: "bad request. something is missing" });
+    } else {
+      bcrypt.hash(password, 10, async (err, hash) => {
+        if (err) {
+          throw err;
+        }
+        try {
+          const result = await User.create({
+            username,
+            email,
+            phone,
+            password: hash,
+          });
+          res.status(201).json({ id: result.id });
+        } catch (err) {
+          res.status(403).json(err);
+        }
+      });
     }
-    const result = await User.create({ username, email, phone, password });
-
-    res.status(201).json({
-      id: result.id,
-    });
-  } catch {
-    (err) => {
-      res.status(403).json(err);
-    };
+  } catch (err) {
+    res.status(403).json(err);
   }
 };
 
 exports.signin = (req, res, next) => {
   User.findOne({ where: { email: req.body.email } }).then((result) => {
     if (result) {
-      if (result.password === req.body.password) {
-        res.json({ message: "Login Successful" });
-      } else {
-        res.status(401).json({ err: "User not authorized" });
-      }
+      bcrypt.compare(
+        req.body.password,
+        result.password,
+        async (err, response) => {
+          if (!err) {
+            res.json({ message: "Login Successful" });
+          } else {
+            res.status(401).json({ err: "User not authorized" });
+          }
+        }
+      );
     } else {
       res.status(404).json({ err: "user not found" });
     }
   });
-  console.log(req.body);
 };
